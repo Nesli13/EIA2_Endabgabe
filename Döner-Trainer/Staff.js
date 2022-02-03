e1874967cf916c44c67d4fb56880d7e8494d2970
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2022-02-03 12:17:10 +0100

    created all Staff-draw-methods

--- Döner-Trainer/Staff.js
+++ Döner-Trainer/Staff.js
@@ -1,2 +1,205 @@
 "use strict";
+var DönerTrainer_Endabgabe;
+(function (DönerTrainer_Endabgabe) {
+    class Staff extends DönerTrainer_Endabgabe.Person {
+        constructor(_position, _size) {
+            super(_position);
+            super(_size);
+            if (_position)
+                this.position = _position;
+            else
+                this.position = new DönerTrainer_Endabgabe.Vector(0, 0);
+        }
+        draw() {
+            //happy Customer
+            //body
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.translate(this.position.x + 100, this.position.y - 400);
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#Dda0dd";
+            DönerTrainer_Endabgabe.crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //head
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //eye
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "black";
+            DönerTrainer_Endabgabe.crc2.arc(73, -30, 4, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(93, -30, 4, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //mouth
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.arc(83, -17, 10, 0, Math.PI, false);
+            DönerTrainer_Endabgabe.crc2.lineWidth = 2;
+            DönerTrainer_Endabgabe.crc2.strokeStyle = "black";
+            DönerTrainer_Endabgabe.crc2.stroke();
+            //arm
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //feet
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#6495ed";
+            DönerTrainer_Endabgabe.crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //hair
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.moveTo(70, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(70, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(75, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(75, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(80, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(80, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(85, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(85, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(90, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(90, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(95, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(95, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(100, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(100, -40);
+            DönerTrainer_Endabgabe.crc2.closePath();
+            DönerTrainer_Endabgabe.crc2.stroke();
+            DönerTrainer_Endabgabe.crc2.restore();
+            DönerTrainer_Endabgabe.crc2.save();
+        }
+        drawSleepyStaff(_position) {
+            //impatient Customer
+            //body
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.translate(_position.x, _position.y);
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#Dda0dd";
+            DönerTrainer_Endabgabe.crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //head
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //eye
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "black";
+            DönerTrainer_Endabgabe.crc2.arc(73, -30, 2, 0, 1 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(93, -30, 2, 0, 1 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //mouth
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.moveTo(71, -15);
+            DönerTrainer_Endabgabe.crc2.lineTo(100, -16);
+            DönerTrainer_Endabgabe.crc2.lineWidth = 2;
+            DönerTrainer_Endabgabe.crc2.strokeStyle = "black";
+            DönerTrainer_Endabgabe.crc2.stroke();
+            //arm
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //feet
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#6495ed";
+            DönerTrainer_Endabgabe.crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //hair
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.moveTo(70, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(70, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(75, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(75, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(80, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(80, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(85, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(85, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(90, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(90, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(95, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(95, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(100, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(100, -40);
+            DönerTrainer_Endabgabe.crc2.closePath();
+            DönerTrainer_Endabgabe.crc2.stroke();
+            DönerTrainer_Endabgabe.crc2.restore();
+            DönerTrainer_Endabgabe.crc2.save();
+        }
+        drawMadStaff(_position) {
+            //mad Customer
+            //body
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.translate(_position.x - 650, _position.y - 400);
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#Dda0dd";
+            DönerTrainer_Endabgabe.crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //head
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.save();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //eye
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "black";
+            DönerTrainer_Endabgabe.crc2.arc(73, -30, 4, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(93, -30, 4, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.fill();
+            //mouth
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.arc(83, -13, 10, 0, Math.PI, true);
+            DönerTrainer_Endabgabe.crc2.lineWidth = 2;
+            DönerTrainer_Endabgabe.crc2.strokeStyle = "black";
+            DönerTrainer_Endabgabe.crc2.stroke();
+            //arm
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#FFEFd5";
+            DönerTrainer_Endabgabe.crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.closePath();
+            DönerTrainer_Endabgabe.crc2.fill();
+            //feet
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.fillStyle = "#6495ed";
+            DönerTrainer_Endabgabe.crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+            DönerTrainer_Endabgabe.crc2.closePath();
+            DönerTrainer_Endabgabe.crc2.fill();
+            //hair
+            DönerTrainer_Endabgabe.crc2.beginPath();
+            DönerTrainer_Endabgabe.crc2.moveTo(70, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(70, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(75, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(75, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(80, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(80, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(85, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(85, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(90, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(90, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(95, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(95, -40);
+            DönerTrainer_Endabgabe.crc2.moveTo(100, -55);
+            DönerTrainer_Endabgabe.crc2.lineTo(100, -40);
+            DönerTrainer_Endabgabe.crc2.closePath();
+            DönerTrainer_Endabgabe.crc2.stroke();
+            DönerTrainer_Endabgabe.crc2.restore();
+            DönerTrainer_Endabgabe.crc2.save();
+        }
+        mood() {
+            console.log("mood");
+        }
+        move() {
+            console.log("mood");
+        }
+    }
+    DönerTrainer_Endabgabe.Staff = Staff;
+})(DönerTrainer_Endabgabe || (DönerTrainer_Endabgabe = {}));
 //# sourceMappingURL=Staff.js.map
--- Döner-Trainer/Staff.ts
+++ Döner-Trainer/Staff.ts
@@ -1,3 +1,228 @@
 namespace DönerTrainer_Endabgabe {
+    export class Staff extends Person {
 
+        constructor(_position: Vector, _size: Vector) {
+            super(_position);
+            super(_size);
+            if (_position)
+                this.position = _position;
+            else
+                this.position = new Vector(0, 0);
+
+        }
+
+        public draw(): void {
+            
+                //happy Customer
+                //body
+                crc2.beginPath();
+                crc2.save();
+                crc2.translate(this.position.x + 100, this.position.y - 400);
+                crc2.fillStyle = "#Dda0dd";
+                crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //head
+                crc2.beginPath();
+                crc2.save();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+                crc2.fill();
+        
+                //eye
+                crc2.beginPath();
+                crc2.fillStyle = "black";
+                crc2.arc(73, -30, 4, 0, 2 * Math.PI);
+                crc2.arc(93, -30, 4, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //mouth
+                crc2.beginPath();
+                crc2.arc(83, -17, 10, 0, Math.PI, false);
+                crc2.lineWidth = 2;
+                crc2.strokeStyle = "black";
+                crc2.stroke();
+        
+                //arm
+                crc2.beginPath();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+                crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //feet
+                crc2.beginPath();
+                crc2.fillStyle = "#6495ed";
+                crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+                crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //hair
+                crc2.beginPath();
+                crc2.moveTo(70, -55);
+                crc2.lineTo(70, -40);
+                crc2.moveTo(75, -55);
+                crc2.lineTo(75, -40);
+                crc2.moveTo(80, -55);
+                crc2.lineTo(80, -40);
+                crc2.moveTo(85, -55);
+                crc2.lineTo(85, -40);
+                crc2.moveTo(90, -55);
+                crc2.lineTo(90, -40);
+                crc2.moveTo(95, -55);
+                crc2.lineTo(95, -40);
+                crc2.moveTo(100, -55);
+                crc2.lineTo(100, -40);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.restore();
+                crc2.save();
+        
+            }
+            public drawSleepyStaff(_position: Vector): void {
+                //impatient Customer
+                //body
+                crc2.beginPath();
+                crc2.save();
+                crc2.translate(_position.x, _position.y);
+                crc2.fillStyle = "#Dda0dd";
+                crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+                crc2.fill();
+        
+        
+                //head
+                crc2.beginPath();
+                crc2.save();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+                crc2.fill();
+        
+                //eye
+                crc2.beginPath();
+                crc2.fillStyle = "black";
+                crc2.arc(73, -30, 2, 0, 1 * Math.PI);
+                crc2.arc(93, -30, 2, 0, 1 * Math.PI);
+                crc2.fill();
+        
+                //mouth
+                crc2.beginPath();
+                crc2.moveTo(71, -15);
+                crc2.lineTo(100, -16);
+                crc2.lineWidth = 2;
+                crc2.strokeStyle = "black";
+                crc2.stroke();
+        
+                //arm
+                crc2.beginPath();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+                crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //feet
+                crc2.beginPath();
+                crc2.fillStyle = "#6495ed";
+                crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+                crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //hair
+                crc2.beginPath();
+                crc2.moveTo(70, -55);
+                crc2.lineTo(70, -40);
+                crc2.moveTo(75, -55);
+                crc2.lineTo(75, -40);
+                crc2.moveTo(80, -55);
+                crc2.lineTo(80, -40);
+                crc2.moveTo(85, -55);
+                crc2.lineTo(85, -40);
+                crc2.moveTo(90, -55);
+                crc2.lineTo(90, -40);
+                crc2.moveTo(95, -55);
+                crc2.lineTo(95, -40);
+                crc2.moveTo(100, -55);
+                crc2.lineTo(100, -40);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.restore();
+                crc2.save();
+        
+            }
+            public drawMadStaff(_position: Vector): void {
+                //mad Customer
+                //body
+                crc2.beginPath();
+                crc2.save();
+                crc2.translate(_position.x - 650, _position.y - 400);
+                crc2.fillStyle = "#Dda0dd";
+                crc2.arc(85, 25, 35, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //head
+                crc2.beginPath();
+                crc2.save();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(85, -30, 25, -6, 2 * Math.PI);
+                crc2.fill();
+        
+                //eye
+                crc2.beginPath();
+                crc2.fillStyle = "black";
+                crc2.arc(73, -30, 4, 0, 2 * Math.PI);
+                crc2.arc(93, -30, 4, 0, 2 * Math.PI);
+                crc2.fill();
+        
+                //mouth
+                crc2.beginPath();
+                crc2.arc(83, -13, 10, 0, Math.PI, true);
+                crc2.lineWidth = 2;
+                crc2.strokeStyle = "black";
+                crc2.stroke();
+        
+                //arm
+                crc2.beginPath();
+                crc2.fillStyle = "#FFEFd5";
+                crc2.arc(63, 20, 7, 0, 2 * Math.PI);
+                crc2.arc(105, 20, 7, 0, 2 * Math.PI);
+                crc2.closePath();
+                crc2.fill();
+        
+                //feet
+                crc2.beginPath();
+                crc2.fillStyle = "#6495ed";
+                crc2.arc(70, 60, 8, 0, 2 * Math.PI);
+                crc2.arc(95, 60, 8, 0, 2 * Math.PI);
+                crc2.closePath();
+                crc2.fill();
+        
+                //hair
+                crc2.beginPath();
+                crc2.moveTo(70, -55);
+                crc2.lineTo(70, -40);
+                crc2.moveTo(75, -55);
+                crc2.lineTo(75, -40);
+                crc2.moveTo(80, -55);
+                crc2.lineTo(80, -40);
+                crc2.moveTo(85, -55);
+                crc2.lineTo(85, -40);
+                crc2.moveTo(90, -55);
+                crc2.lineTo(90, -40);
+                crc2.moveTo(95, -55);
+                crc2.lineTo(95, -40);
+                crc2.moveTo(100, -55);
+                crc2.lineTo(100, -40);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.restore();
+                crc2.save();
+            }
+        
+
+        public mood(): void {
+            console.log("mood");
+        }
+        public move(): void {
+            console.log("mood");
+        }
+    }
 }
